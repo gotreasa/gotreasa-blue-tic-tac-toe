9015c972861852fed27e02427c1528351d25583b
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-04-03 20:30:51 +0100

    feat(us1/uat1.1): 🟢 green

--- test/domain/Game.spec.js
+++ test/domain/Game.spec.js
@@ -7,7 +7,11 @@ describe('Starting a Game', () => {
     game = new Game();
   });
 
-  test('should have an 9 empty square', () => {
+  test('should have an 9 empty squares', () => {
     expect(game.squares).toEqual([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']);
+  });
+
+  test("should the board's empty state", () => {
+    expect(game.getGrid()).toBe(' | | \n-+-+-\n | | \n-+-+-\n | | ');
   });
 });
