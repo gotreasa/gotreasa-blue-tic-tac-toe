6b0cfa63c8ac767402e5d4dc677cc3cdc6125fdf
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-04-08 00:27:32 +0100

    refactor(us5/uat5.3): 👩‍💻 moved the position constants into their own file

---
+++ src/constants/positions.js
@@ -0,0 +1,17 @@
+const LEFT_COLUMN = [0, 1, 2];
+const MIDDLE_COLUMN = [3, 4, 5];
+const RGHT_COLUMN = [6, 7, 8];
+const TOP_ROW = [0, 3, 6];
+const MIDDLE_ROW = [1, 4, 7];
+const BOTTOM_ROW = [2, 5, 8];
+const BACKWARD_DIAGONAL = [0, 4, 8];
+
+module.exports = {
+  LEFT_COLUMN,
+  MIDDLE_COLUMN,
+  RGHT_COLUMN,
+  TOP_ROW,
+  MIDDLE_ROW,
+  BOTTOM_ROW,
+  BACKWARD_DIAGONAL,
+};
--- src/domain/Game.js
+++ src/domain/Game.js
@@ -1,12 +1,14 @@
 const { Board } = require('./Board');
+const {
+  LEFT_COLUMN,
+  MIDDLE_COLUMN,
+  RGHT_COLUMN,
+  TOP_ROW,
+  MIDDLE_ROW,
+  BOTTOM_ROW,
+  BACKWARD_DIAGONAL,
+} = require('../constants/positions');
 
-const LEFT_COLUMN = [0, 1, 2];
-const MIDDLE_COLUMN = [3, 4, 5];
-const RGHT_COLUMN = [6, 7, 8];
-const TOP_ROW = [0, 3, 6];
-const MIDDLE_ROW = [1, 4, 7];
-const BOTTOM_ROW = [2, 5, 8];
-const BACKWARD_DIAGONAL = [0, 4, 8];
 class Game {
   constructor() {
     this.board = new Board();
