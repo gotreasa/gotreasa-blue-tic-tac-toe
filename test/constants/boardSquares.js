8285cad0ae1a7226405cd5468b4817370f148311
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-04-08 00:15:55 +0100

    refactor(us5/uat5.3): 👩‍💻 moved the constants to a dedicated file

---
+++ test/constants/boardSquares.js
@@ -0,0 +1,19 @@
+const TOP_ROW_X_WIN = ['X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '];
+const MIDDLE_ROW_O_WIN = [' ', 'X', 'X', 'O', 'O', 'O', 'X', ' ', ' '];
+const BOTTOM_ROW_X_WIN = [' ', 'O', 'O', ' ', ' ', ' ', 'X', 'X', 'X'];
+const LEFT_COLUMN_X_WIN = ['X', 'O', ' ', 'X', 'O', ' ', 'X', ' ', ' '];
+const MIDDLE_COLUMN_O_WIN = [' ', 'O', 'X', 'X', 'O', ' ', 'X', 'O', ' '];
+const RIGHT_COLUMN_X_WIN = [' ', 'O', 'X', ' ', 'O', 'X', ' ', ' ', 'X'];
+const BACKWARD_DIAGONAL_X_WIN = ['X', 'O', ' ', ' ', 'X', ' ', 'O', ' ', 'X'];
+const FORWARD_DIAGONAL_O_WIN = [' ', 'X', 'O', 'X', 'O', ' ', 'O', 'X', ' '];
+
+module.exports = {
+  TOP_ROW_X_WIN,
+  MIDDLE_ROW_O_WIN,
+  BOTTOM_ROW_X_WIN,
+  LEFT_COLUMN_X_WIN,
+  MIDDLE_COLUMN_O_WIN,
+  RIGHT_COLUMN_X_WIN,
+  BACKWARD_DIAGONAL_X_WIN,
+  FORWARD_DIAGONAL_O_WIN,
+};
--- test/domain/Game.spec.js
+++ test/domain/Game.spec.js
@@ -1,13 +1,14 @@
 const { Game } = require('../../src/domain/Game');
-
-const TOP_ROW_X_WIN = ['X', 'X', 'X', 'O', 'O', ' ', ' ', ' ', ' '];
-const MIDDLE_ROW_O_WIN = [' ', 'X', 'X', 'O', 'O', 'O', 'X', ' ', ' '];
-const BOTTOM_ROW_X_WIN = [' ', 'O', 'O', ' ', ' ', ' ', 'X', 'X', 'X'];
-const LEFT_COLUMN_X_WIN = ['X', 'O', ' ', 'X', 'O', ' ', 'X', ' ', ' '];
-const MIDDLE_COLUMN_O_WIN = [' ', 'O', 'X', 'X', 'O', ' ', 'X', 'O', ' '];
-const RIGHT_COLUMN_X_WIN = [' ', 'O', 'X', ' ', 'O', 'X', ' ', ' ', 'X'];
-const BACKWARD_DIAGONAL_X_WIN = ['X', 'O', ' ', ' ', 'X', ' ', 'O', ' ', 'X'];
-const FORWARD_DIAGONAL_O_WIN = [' ', 'X', 'O', 'X', 'O', ' ', 'O', 'X', ' '];
+const {
+  TOP_ROW_X_WIN,
+  MIDDLE_ROW_O_WIN,
+  BOTTOM_ROW_X_WIN,
+  LEFT_COLUMN_X_WIN,
+  MIDDLE_COLUMN_O_WIN,
+  RIGHT_COLUMN_X_WIN,
+  BACKWARD_DIAGONAL_X_WIN,
+  FORWARD_DIAGONAL_O_WIN,
+} = require('../constants/boardSquares');
 
 const EMPTY_SQUARES = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
