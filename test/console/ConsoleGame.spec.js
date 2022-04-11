345477013035c0622bdc1aba08e0c3704a8f4b10
Gearoid O'Treasaigh <gearoid.otreasaigh@ibm.com>
2022-04-11 18:24:40 +0100

    feat(us9/uat9.2): 🔴 red

--- test/console/ConsoleGame.spec.js
+++ test/console/ConsoleGame.spec.js
@@ -6,6 +6,7 @@ describe('Console Game', () => {
   beforeEach(() => {
     consoleGame = new ConsoleGame();
     consoleGame.controller.move = jest.fn();
+    consoleGame.renderer.print = jest.fn();
   });
 
   afterEach(() => {
@@ -15,5 +16,10 @@ describe('Console Game', () => {
   test('should make a move when the game is played', () => {
     consoleGame.play();
     expect(consoleGame.controller.move).toHaveBeenCalled();
+  });
+
+  test('should make a print the state when the game is played', () => {
+    consoleGame.play();
+    expect(consoleGame.renderer.print).toHaveBeenCalled();
   });
 });
